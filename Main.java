1ee039d1efe88b406f2c418c8b5d438a61a2a42e
Katherine Cummings <bobkat1252@gmail.com>
2019-02-05 17:40:40 -0500

    Alice and Bob (complete)
    finished version of alice and bob lab.

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
+        String x = "alice";
+        String y = "bob";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hi user, what's your name?");
+        String name = input.nextLine();
+        name = name.toLowerCase();
+        
+        if(name.equals(x) || name.equals(y)){
+            System.out.println("Hello " + name + "!");
+        }
+        else{
+            System.out.println("Who are you?!?!?!");
+        }
     }
 }
